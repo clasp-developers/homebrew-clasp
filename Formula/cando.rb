39beed50a33bb46e18e266ce485a28a7e6c0a7d0
Tarn W. Burton <twburton@gmail.com>
2022-05-29 14:35:30 -0400

    Add formula

---
+++ Formula/cando.rb
@@ -0,0 +1,32 @@
+class Cando < Formula
+  desc "Common Lisp implementation and Cando chemistry system"
+  homepage "https://github.com/clasp-developers/clasp"
+  url "https://github.com/clasp-developers/clasp.git", using: :git, branch: "main"
+  version "1.0.0-220-g0f62839c7"
+  license "GPL-2.0-or-later"
+
+  depends_on "boost" => :build
+  depends_on "libunwind-headers" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+  depends_on "sbcl" => :build
+  depends_on "expat"
+  depends_on "fmt"
+  depends_on "gmp"
+  depends_on "jupyterlab"
+  depends_on "llvm"
+  depends_on "netcdf"
+  uses_from_macos "libffi"
+
+  def install
+    ENV.deparallelize
+    system "./koga", "--bin-path=#{bin}", "--share-path=#{share}/clasp/", "--lib-path=#{lib}/clasp/",
+      "--jupyter-path=#{share}/jupyter/", "--jupyter", "--extensions=cando,seqan-clasp"
+    system "ninja", "-C", "build"
+    system "ninja", "-C", "build", "install"
+  end
+
+  test do
+    assert_match "clasp-boehmprecise-1.0.0", shell_output("#{bin}/clasp --version")
+  end
+end
