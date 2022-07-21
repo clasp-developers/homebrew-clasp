152471a6e12760e0c19414a644a7f1025374e32d
yitzchak <yitzchak@users.noreply.github.com>
2022-07-21 03:30:32 +0000

    Bump cando version

--- Formula/cando.rb
+++ Formula/cando.rb
@@ -3,8 +3,8 @@ class Cando < Formula
   homepage "https://github.com/clasp-developers/clasp"
   url "https://github.com/clasp-developers/clasp.git",
       using:    :git,
-      revision: "c26a23a09a7f3601a7784066260df242e5ec0e35"
-  version "1.0.0-330-gc26a23a09-g4eb58b60"
+      revision: "d4314a4ec1abe092f96fd22ff2decc14bb405d3d"
+  version "1.0.0-407-gd4314a4ec-g82438caa"
   license "GPL-2.0-or-later"
   head "https://github.com/clasp-developers/clasp.git",
        using:  :git,
