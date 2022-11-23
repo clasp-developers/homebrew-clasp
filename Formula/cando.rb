8d8a3913746e0ae0ea1009c89ff9dc446e702772
Tarn W. Burton <twburton@gmail.com>
2022-11-23 14:40:01 +0000

    cando: update 2.0.0-95-g307feaf9e-gd84087a4 bottle.

--- Formula/cando.rb
+++ Formula/cando.rb
@@ -10,9 +10,9 @@ class Cando < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/clasp-developers/clasp"
-    sha256 monterey:     "801b608a3c439b2f7514467bda5df94ba9346fc325b6bc0e02e69779414dc88e"
-    sha256 big_sur:      "3df8e1f8e11d5765a0674f4491df214e28700cbee335beb0090ee700fa88b8f7"
-    sha256 x86_64_linux: "ece7806c7b9b8abd80e8dd2b1c2b6e5c25c2fc1ca8c1e122a647e85de2b467ec"
+    sha256 monterey:     "1940fa0591c6d21b50bd03dc00bb81020b0334c3f0bab4cd757f360666cb5a46"
+    sha256 big_sur:      "48b77559c9f2db69fed8aa89c4ac8f00eb85145d6af32c889e381d8e5526e261"
+    sha256 x86_64_linux: "707fde7cfbe2e459c8cfceee20658da56f58fc9c1556b4d0645f668d864715d6"
   end
 
   depends_on "boost" => :build
