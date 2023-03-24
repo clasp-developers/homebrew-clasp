cef40ef6501f98b38116a315c6e91c65845b8428
yitzchak <yitzchak@users.noreply.github.com>
2023-03-24 02:29:31 +0000

    clasp-cl 2.2.0-63-ge3ddb3cf9
    Build clasp-cl
    
    Closes #277.
    
    Signed-off-by: Tarn W. Burton <twburton@gmail.com>

--- Formula/clasp-cl.rb
+++ Formula/clasp-cl.rb
@@ -3,8 +3,8 @@ class ClaspCl < Formula
   homepage "https://github.com/clasp-developers/clasp"
   url "https://github.com/clasp-developers/clasp.git",
       using:    :git,
-      revision: "49c0b641f5fad52b753ea3a75eaa2e7a7f87251d"
-  version "2.2.0-62-g49c0b641f"
+      revision: "e3ddb3cf93d1f43c984f29b7691884f5f69ab4f2"
+  version "2.2.0-63-ge3ddb3cf9"
   license "GPL-2.0-or-later"
   head "https://github.com/clasp-developers/clasp.git", branch: "main"
 
