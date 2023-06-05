1ee6c64f7bd40d22cd9eadd986ea3756226d95b1
yitzchak <yitzchak@users.noreply.github.com>
2023-06-05 02:46:26 +0000

    Build cando
    Closes #324.
    
    Signed-off-by: Tarn W. Burton <twburton@gmail.com>

--- Formula/cando.rb
+++ Formula/cando.rb
@@ -3,8 +3,8 @@ class Cando < Formula
   homepage "https://github.com/clasp-developers/clasp"
   url "https://github.com/clasp-developers/clasp.git",
       using:    :git,
-      revision: "a91f0ced4d46a432b7edb7a805db77d3030efb08"
-  version "2.2.0-181-ga91f0ced4-ge8d1372f"
+      revision: "d6f2ae2f510d87aa3fa1f6387eb3872fd378f062"
+  version "2.3.0-1-gd6f2ae2f5-ge8d1372f"
   license "GPL-2.0-or-later"
   head "https://github.com/clasp-developers/clasp.git", branch: "main"
 
