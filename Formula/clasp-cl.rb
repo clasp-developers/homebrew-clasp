192f9e4cb802077dff20f4e7edc0a17df2fd5bc7
yitzchak <yitzchak@users.noreply.github.com>
2023-06-16 16:48:17 +0000

    Build clasp-cl

--- Formula/clasp-cl.rb
+++ Formula/clasp-cl.rb
@@ -3,8 +3,8 @@ class ClaspCl < Formula
   homepage "https://github.com/clasp-developers/clasp"
   url "https://github.com/clasp-developers/clasp.git",
       using:    :git,
-      revision: "1c6d486bc8e6c16b5ae10aa161bb39be043676a2"
-  version "2.3.0-20-g1c6d486bc"
+      revision: "e6298914bda9d19befc11424566f5e7c636cc50c"
+  version "2.3.0-25-ge6298914b"
   license "GPL-2.0-or-later"
   head "https://github.com/clasp-developers/clasp.git", branch: "main"
 
