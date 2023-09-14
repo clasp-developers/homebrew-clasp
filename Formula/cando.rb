f46325013a4b9ecb765dc082c07edabd7baf1c59
yitzchak <yitzchak@users.noreply.github.com>
2023-09-14 02:13:40 +0000

    Build cando

--- Formula/cando.rb
+++ Formula/cando.rb
@@ -3,8 +3,8 @@ class Cando < Formula
   homepage "https://github.com/clasp-developers/clasp"
   url "https://github.com/clasp-developers/clasp.git",
       using:    :git,
-      revision: "7e81a6a287eda2fa758bfd6750254ecc32221b6e"
-  version "2.3.0-143-g7e81a6a28-g2ba68d24"
+      revision: "6dcee658888a06aaa64abc82691edbcc83d226e4"
+  version "2.3.0-165-g6dcee6588-g2ba68d24"
   license "GPL-2.0-or-later"
   head "https://github.com/clasp-developers/clasp.git", branch: "main"
 
