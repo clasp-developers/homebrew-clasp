893974be2450f9384c50bc18d15fc7b9a04599a6
Tarn W. Burton <twburton@gmail.com>
2024-05-06 10:28:03 +0000

    clasp-cl: update 2.5.0-235-ge805dd64a bottle.

--- Formula/clasp-cl.rb
+++ Formula/clasp-cl.rb
@@ -10,7 +10,7 @@ class ClaspCl < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/clasp-developers/clasp"
-    sha256 monterey: "5b35867f3d89d589ca8f929316b02bd91c5b6aa3235bbf0ed27c083e9959f6eb"
+    sha256 monterey: "d361b1cf2be1f0f73ad37f43db94ef7b47188a3f089da6fded1840570ac50d29"
   end
 
   depends_on "boost" => :build
