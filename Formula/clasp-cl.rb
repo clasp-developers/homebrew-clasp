fcf5824ceefac0b09782ac32a70fe9400a8a73bc
Tarn W. Burton <twburton@gmail.com>
2024-09-07 08:41:54 -0400

    Fix llvm-config path

--- Formula/clasp-cl.rb
+++ Formula/clasp-cl.rb
@@ -27,12 +27,12 @@ class ClaspCl < Formula
   def install
     ENV.deparallelize
     system "./koga", "--reproducible-build", "--bin-path=#{bin}", "--share-path=#{share}/clasp/",
-      "--lib-path=#{lib}/clasp/", "--llvm-config=#{Formula["llvm@17"].opt_bin}/llvm-config",
+      "--lib-path=#{lib}/clasp/", "--llvm-config=#{Formula["llvm@18"].opt_bin}/llvm-config",
       "--pkg-config=#{Formula["pkg-config"].opt_bin}/pkg-config",
       "--cxxflags=-I#{Formula["boost"].include}/", "--cppflags=-I#{Formula["boost"].include}/",
       "--skip-sync=ansi-test,mps,cl-bench,cl-who" if Hardware::CPU.intel?
     system "./koga", "--reproducible-build", "--bin-path=#{bin}", "--share-path=#{share}/clasp/",
-      "--lib-path=#{lib}/clasp/", "--llvm-config=#{Formula["llvm@17"].opt_bin}/llvm-config",
+      "--lib-path=#{lib}/clasp/", "--llvm-config=#{Formula["llvm@18"].opt_bin}/llvm-config",
       "--pkg-config=#{Formula["pkg-config"].opt_bin}/pkg-config",
       "--cxxflags=-I#{Formula["boost"].include}/", "--cppflags=-I#{Formula["boost"].include}/",
       "--skip-sync=ansi-test,mps,cl-bench,cl-who", "--build-mode=bytecode" if Hardware::CPU.arm?
