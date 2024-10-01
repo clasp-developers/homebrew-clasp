1aecbaa83bd89803e794fd459c0c1225af3fa9db
Tarn W. Burton <twburton@gmail.com>
2024-10-01 14:01:28 -0400

    Update to LLVM19

--- Formula/cando.rb
+++ Formula/cando.rb
@@ -21,7 +21,7 @@ class Cando < Formula
   depends_on "fmt"
   depends_on "gmp"
   depends_on "jupyterlab"
-  depends_on "llvm@18"
+  depends_on "llvm@19"
   depends_on "netcdf"
   depends_on "ninja"
   depends_on "zeromq"
@@ -34,14 +34,14 @@ class Cando < Formula
     ENV.deparallelize
     if Hardware::CPU.arm?
       system "./koga", "--reproducible-build", "--bin-path=#{bin}", "--share-path=#{share}/clasp/",
-        "--lib-path=#{lib}/clasp/", "--llvm-config=#{Formula["llvm@18"].opt_bin}/llvm-config",
+        "--lib-path=#{lib}/clasp/", "--llvm-config=#{Formula["llvm@19"].opt_bin}/llvm-config",
         "--pkg-config=#{Formula["pkg-config"].opt_bin}/pkg-config",
         "--cxxflags=-I#{Formula["boost"].include}/", "--cppflags=-I#{Formula["boost"].include}/",
         "--skip-sync=ansi-test,mps,cl-bench,cl-who", "--extensions=cando,seqan-clasp",
         "--build-mode=bytecode"
     else
       system "./koga", "--reproducible-build", "--bin-path=#{bin}", "--share-path=#{share}/clasp/",
-        "--lib-path=#{lib}/clasp/", "--llvm-config=#{Formula["llvm@18"].opt_bin}/llvm-config",
+        "--lib-path=#{lib}/clasp/", "--llvm-config=#{Formula["llvm@19"].opt_bin}/llvm-config",
         "--pkg-config=#{Formula["pkg-config"].opt_bin}/pkg-config",
         "--cxxflags=-I#{Formula["boost"].include}/", "--cppflags=-I#{Formula["boost"].include}/",
         "--skip-sync=ansi-test,mps,cl-bench,cl-who", "--extensions=cando,seqan-clasp"
--- Formula/clasp-cl.rb
+++ Formula/clasp-cl.rb
@@ -21,7 +21,7 @@ class ClaspCl < Formula
   depends_on "sbcl" => :build
   depends_on "fmt"
   depends_on "gmp"
-  depends_on "llvm@18"
+  depends_on "llvm@19"
 
   conflicts_with "cando", because: "both install `clasp` binaries"
 
@@ -29,13 +29,13 @@ class ClaspCl < Formula
     ENV.deparallelize
     if Hardware::CPU.arm?
       system "./koga", "--reproducible-build", "--bin-path=#{bin}", "--share-path=#{share}/clasp/",
-        "--lib-path=#{lib}/clasp/", "--llvm-config=#{Formula["llvm@18"].opt_bin}/llvm-config",
+        "--lib-path=#{lib}/clasp/", "--llvm-config=#{Formula["llvm@19"].opt_bin}/llvm-config",
         "--pkg-config=#{Formula["pkg-config"].opt_bin}/pkg-config",
         "--cxxflags=-I#{Formula["boost"].include}/", "--cppflags=-I#{Formula["boost"].include}/",
         "--skip-sync=ansi-test,mps,cl-bench,cl-who", "--build-mode=bytecode"
     else
       system "./koga", "--reproducible-build", "--bin-path=#{bin}", "--share-path=#{share}/clasp/",
-        "--lib-path=#{lib}/clasp/", "--llvm-config=#{Formula["llvm@18"].opt_bin}/llvm-config",
+        "--lib-path=#{lib}/clasp/", "--llvm-config=#{Formula["llvm@19"].opt_bin}/llvm-config",
         "--pkg-config=#{Formula["pkg-config"].opt_bin}/pkg-config",
         "--cxxflags=-I#{Formula["boost"].include}/", "--cppflags=-I#{Formula["boost"].include}/",
         "--skip-sync=ansi-test,mps,cl-bench,cl-who"
